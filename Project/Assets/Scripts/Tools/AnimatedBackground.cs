e066c463246868391b99b27bf21a83053b01c450
Evgeniy <jakedesu@gmail.com>
2014-08-19 15:51:21 +0400

    Black dongs

---
+++ Project/Assets/Scripts/Tools/AnimatedBackground.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatedBackground : MonoBehaviour {
+
+	public int materialIndex = 0;
+	public Vector2 AnimationRate = new Vector2 (0.0f, 1.0f);
+	public string TextureName = "_MainTex";
+
+	private Vector2 offset = Vector2.zero;
+
+	// Use this for initialization
+	void Start () {
+		renderer.material.mainTexture.wrapMode = TextureWrapMode.Repeat;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		offset += (AnimationRate * Time.deltaTime);
+
+		if (renderer.enabled)
+		{
+			renderer.materials[ materialIndex ].SetTextureOffset( TextureName, offset);
+		}
+	}
+}
