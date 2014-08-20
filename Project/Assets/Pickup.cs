de90ed0d706d973f304dd12bbc5480964d6c0558
Evgeniy <jakedesu@gmail.com>
2014-08-20 15:53:28 +0400

    Weapon Pickup System

---
+++ Project/Assets/Pickup.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup : MonoBehaviour {
+
+	public string Category;
+	public GameObject weapon;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D coll)
+	{
+		if (coll.gameObject.tag == "Player") 
+		{
+			coll.gameObject.SendMessage ("ChangeWeapon", weapon);
+			Destroy (this.gameObject);
+		}
+	}
+}
