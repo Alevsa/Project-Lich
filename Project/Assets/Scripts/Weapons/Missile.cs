0715c6c1918a6cfb6b464b24de8f71dfba112ce6
Sam Rowlands <srowlands@hotmail.co.uk>
2014-08-20 15:06:09 +0100

    Missile Launcher Weapon

---
+++ Project/Assets/Scripts/Weapons/Missile.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Missile : Projectile 
+{
+    public GameObject explosion;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetPosition();
+        StrafeUp();
+        Move();
+	}
+
+    public override void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Enemy")
+        {
+            coll.gameObject.SendMessage("ApplyDamage", Damage);
+            Instantiate(explosion, this.transform.position, Quaternion.identity);
+            Die();
+        }
+    }     
+}
---
+++ Project/Assets/Scripts/Weapons/MissileLauncher.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissileLauncher : Weapon {
+
+	// Use this for initialization
+	void Start () {
+
+        firingPosition = GameObject.Find("WeaponSlot1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    override public void Fire()
+    {
+        Instantiate(projectile, firingPosition.transform.position, Quaternion.identity);
+    }
+}
