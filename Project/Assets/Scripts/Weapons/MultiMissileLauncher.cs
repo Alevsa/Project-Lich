6499ffa73d674f741e0535b4cb1d99700a62eff0
Sam Rowlands <srowlands@hotmail.co.uk>
2014-08-20 16:20:01 +0100

    MultiMissileLauncher

---
+++ Project/Assets/Scripts/Weapons/MultiMissileLauncher.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MultiMissileLauncher : Weapon 
+{
+    public int NumberOfMissiles;
+
+	// Use this for initialization
+	void Start () 
+    {
+        firingPosition = GameObject.Find("WeaponSlot1");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        for (int i = 0; i < NumberOfMissiles; i++)
+        {
+          Instantiate(projectile, firingPosition.transform.position, Quaternion.identity);
+        }
+	}
+}
