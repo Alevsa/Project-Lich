699a88e3a6034dced45cb51bf992096fe9a94d5c
Sam Rowlands <srowlands@hotmail.co.uk>
2014-08-21 00:15:47 +0100

    Added KameCannon.
    Hold down space to keep firing, runs out after certain amount of time.

---
+++ Project/Assets/Scripts/Weapons/KameCannon.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KameCannon : Weapon 
+{
+    private Object kameClone;
+    public float totalEnergy = 5f;
+    private bool firing;
+
+	// Use this for initialization
+	void Start () 
+    {
+        firingPosition = GameObject.Find("WeaponSlot1");	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    { 
+        if (firing)
+            totalEnergy -= Time.deltaTime;
+
+        if (kameClone == null)
+            firing = false;
+	}
+
+    public override void Fire()
+    {
+        if(kameClone == null && totalEnergy > 0) 
+        {
+            kameClone = Instantiate(projectile, firingPosition.transform.position, Quaternion.identity);
+            firing = true;
+        }
+    }
+
+}
---
+++ Project/Assets/Scripts/Weapons/KameCannonProjectile.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KameCannonProjectile : Projectile 
+{
+    private KameCannon kameCannon;
+
+	// Use this for initialization
+	void Start () 
+    {
+        kameCannon = GameObject.Find("KameCannon(Clone)").GetComponent<KameCannon>();
+        this.transform.parent = GameObject.Find("KameCannon(Clone)").transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetButtonUp("Space"))
+            Destroy(gameObject);
+
+        if (kameCannon.totalEnergy <= 0)
+            Destroy(gameObject);
+
+    }
+}
