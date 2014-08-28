8aa7bc50f9991ffe72df4e12a1d9e29a2cb6dcb7
Sam Rowlands <srowlands@hotmail.co.uk>
2014-08-28 11:36:28 +0100

    added level borders and a kill enemy script

---
+++ Project/Assets/Scripts/DestroyLeavingEnemies.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyLeavingEnemies : MonoBehaviour 
+{
+    public bool bottomCollider;
+    public float timeTillDestroyed;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (bottomCollider)
+        {
+            if (col.gameObject.tag == "Enemy")
+            {
+                StartCoroutine(DestroyEnemy(col.gameObject));
+            }
+        }
+    }
+
+    IEnumerator DestroyEnemy(GameObject col)
+    {
+        yield return new WaitForSeconds(timeTillDestroyed);
+        Destroy(col);
+    }
+}
