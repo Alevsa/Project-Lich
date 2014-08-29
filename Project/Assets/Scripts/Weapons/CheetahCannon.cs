956f0ebf9c17df552334c88cca5b71a9d06e0634
Sam Rowlands <srowlands@hotmail.co.uk>
2014-08-29 13:30:05 +0100

    Cheetah Cannon :tiger:

---
+++ Project/Assets/Scripts/Weapons/CheetahCannon.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheetahCannon : Weapon 
+{
+
+	// Use this for initialization
+	void Start () 
+    {
+        firingPosition = GameObject.Find("WeaponSlot1");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+
+    public override void Fire()
+    {
+        Instantiate(projectile, firingPosition.transform.position, Quaternion.identity); 
+    }
+}
---
+++ Project/Assets/Scripts/Weapons/CheetahProjectile.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheetahProjectile : Projectile {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        GetPosition();
+        StrafeUp();
+        Move();
+        DestroyAfterTime();
+	}
+}
