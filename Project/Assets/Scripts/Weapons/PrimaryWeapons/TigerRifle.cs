a79b241b4025bbd7bfccb55604eb7201a743f87c
Sam Rowlands <srowlands@hotmail.co.uk>
2014-09-01 11:51:21 +0100

    began the Tiger Rifle - new main weapon.
    Bigger and slower than lion cannon, but will do more damage and hit more
    than 1 enemy in a row.

---
+++ Project/Assets/Scripts/Weapons/PrimaryWeapons/TigerRifle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TigerRifle : Weapon 
+{
+
+	// Use this for initialization
+	void Start () 
+    {
+
+        firingPosition = GameObject.Find("WeaponSlot1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public override void Fire()
+    {
+        Instantiate(projectile, firingPosition.transform.position, Quaternion.identity); 
+    }
+}
---
+++ Project/Assets/Scripts/Weapons/PrimaryWeapons/TigerRifleProjectile.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TigerRifleProjectile : Projectile 
+{
+    public int enemiesHit;
+    private int hitCounter;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        GetPosition();
+        StrafeUp();
+        Move();
+        DestroyAfterTime();
+
+        if (hitCounter >=    enemiesHit)
+            Die();
+	}
+
+    public override void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Enemy")
+        {
+            hitCounter++;
+            coll.gameObject.SendMessage("ApplyDamage", Damage);
+            coll.gameObject.SendMessage("SpawnExplosion", transform.position);
+        }
+        
+    }
+}
