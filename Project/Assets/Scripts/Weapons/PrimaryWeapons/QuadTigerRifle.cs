5ae6b520e714f8a66663f29970be143a0ac7afac
Sam Rowlands <srowlands@hotmail.co.uk>
2014-09-02 14:12:54 +0100

    Added Upgraded Tiger Rifle and Cheetah Gun - All :tiger:
    yolo

---
+++ Project/Assets/Scripts/Weapons/PrimaryWeapons/QuadTigerRifle.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuadTigerRifle : Weapon
+{
+
+	// Use this for initialization
+	void Start () 
+    {
+        firingPosition = GameObject.Find("WeaponSlot1");    
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    public override void Fire()
+    {
+        StartCoroutine(FireDouble());
+    }
+
+    IEnumerator FireDouble()
+    {
+        Instantiate(projectile, firingPosition.transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(0.1f);
+        Instantiate(projectile, firingPosition.transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(0.1f);
+        Instantiate(projectile, firingPosition.transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(0.1f);
+        Instantiate(projectile, firingPosition.transform.position, Quaternion.identity);
+    }
+}
--- Project/Assets/Scripts/Weapons/PrimaryWeapons/TigerRifle.cs
+++ Project/Assets/Scripts/Weapons/PrimaryWeapons/TigerRifle.cs
@@ -7,8 +7,7 @@ public class TigerRifle : Weapon
 	// Use this for initialization
 	void Start () 
     {
-
-        firingPosition = GameObject.Find("WeaponSlot1");
+        firingPosition = GameObject.Find("WeaponSlot1");    
 	}
 	
 	// Update is called once per frame
