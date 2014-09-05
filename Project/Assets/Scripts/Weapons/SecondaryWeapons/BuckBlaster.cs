90f7bd57895af6c42dea9ae25581592a4e137f3a
Sam Rowlands <srowlands@hotmail.co.uk>
2014-09-05 18:44:57 +0100

    started buck blaster

---
+++ Project/Assets/Scripts/Weapons/SecondaryWeapons/BuckBlaster.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuckBlaster : Weapon 
+{
+    public int Ammo;
+    public GameObject Slug;
+    private int slugs = 9, counter;
+    private BuckBlasterProjectile slug;
+    private Vector3 direction;
+
+	void Start () 
+    {
+        firingPosition = GameObject.Find("WeaponSlot1");
+	}
+	
+	void Update () {
+	
+	}
+
+    override public void Fire()
+    {
+        for (int i = 0; i < slugs; i++)
+        {
+                Instantiate(Slug, firingPosition.transform.position, Quaternion.identity);  
+        }
+    }
+}
---
+++ Project/Assets/Scripts/Weapons/SecondaryWeapons/BuckBlasterProjectile.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuckBlasterProjectile : Projectile
+{
+    private Vector3 direction;
+
+    public BuckBlasterProjectile(Vector3 direction)
+    {
+        this.direction = direction;
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        GetPosition();
+        StrafeUp();
+        Move();
+        DestroyAfterTime();
+	}
+
+    public void StrafeUp()
+    {
+        movement += (direction * Time.deltaTime);
+    }
+}
