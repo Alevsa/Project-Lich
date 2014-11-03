2d9a54ea838f067c0cf578adbdba6482f1d6b10a
Evgeniy <jakedesu@gmail.com>
2014-11-03 08:51:54 +0300

    Started the UI

---
+++ Project/Assets/Controller.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using MainMenu;
+
+public class Controller : MonoBehaviour {
+
+	public State MenuState = State.Default;
+	public Canvas DefaultCanvas;
+	public Canvas OptionsCanvas;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void StartEndless ()
+	{
+		Application.LoadLevel (1);
+	}
+
+	void OpenOptions ()
+	{
+		MenuState = State.Options;
+	}
+}
---
+++ Project/Assets/Scripts/MainMenuNamespace.cs
@@ -0,0 +1,4 @@
+﻿namespace MainMenu {
+	public enum State {Default, Options};
+}
+
