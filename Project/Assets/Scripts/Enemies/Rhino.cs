e52003d99e937991bae76827c360eafeb461080f
Evgeniy <jakedesu@gmail.com>
2015-01-09 07:05:42 +0300

    THE RHINO
    IS HERE

--- Project/Assets/Scripts/Enemies/Rhino.cs
+++ Project/Assets/Scripts/Enemies/Rhino.cs
@@ -4,17 +4,21 @@ using System.Collections;
 public class Rhino : Enemy {
 
 	public Vector3 DefaultPosition;
+	public float maxPatternPointDistance = 1F;
 	private bool isOnPattern;
 	private ArrayList pattern;
 
+	private Quaternion startRot;
+	private Quaternion targetRot;
+
 	// Use this for initialization
 	void Start () {
-	
+		pattern = new ArrayList ();
 	}
 	
 	// Update is called once per frame
 	public override void Update () {
-		if ((transform.position == DefaultPosition) && !isOnPattern)
+		if (Spatial.isAcceptableDistance(maxPatternPointDistance, this.transform.position, DefaultPosition) && !isOnPattern)
 		{
 			GetPattern ();
 		}
@@ -29,13 +33,13 @@ public class Rhino : Enemy {
 		switch (caseSwitch) 
 		{
 			case 1:
-				pattern.Add (new Vector3 (-3, -6, 0));
-				pattern.Add (new Vector3 (0, -6, 0));
-				pattern.Add (new Vector3 (0, 6, 0));
-				pattern.Add (new Vector3 (3, 6, 0));
-				pattern.Add (new Vector3 (3, -6, 0));
-				pattern.Add (new Vector3 (0, -6, 0));
-				pattern.Add (new Vector3 (0, 6, 0));
+				pattern.Add (new Vector3 (-3, -12, 0));
+				pattern.Add (new Vector3 (0, -12, 0));
+				pattern.Add (new Vector3 (0, 12, 0));
+				pattern.Add (new Vector3 (3, 12, 0));
+				pattern.Add (new Vector3 (3, -12, 0));
+				pattern.Add (new Vector3 (0, -12, 0));
+				pattern.Add (new Vector3 (0, 12, 0));
 				isOnPattern = true;
 				break;
 
@@ -46,16 +50,45 @@ public class Rhino : Enemy {
 
 	private void ExecutePattern()
 	{
-//		if (pattern [0] == null) 
-//		{
-//			isOnPattern = false;
-//			return;
-//		}
-//
-//		if (transform.position != pattern[0])
+		if (pattern.Count == 0) 
+		{
+			isOnPattern = false;
+			return;
+		}
+
+		if (!Spatial.isAcceptableDistance(maxPatternPointDistance, this.transform.position, (Vector3)pattern[0]))
+		{
+			targetRot = Target ((Vector3)pattern [0]);
+			GetRotation ();
+			Rotate ();
+			Move ();
+		}
+		 else
+			pattern.RemoveAt (0);
 
 	}
 
+	void GetRotation() {
+		startRot = transform.rotation;
+	}
 
+	public override void Move() {
+		transform.Translate (Vector3.up * Time.deltaTime * Speed);
+	}
+
+	void Rotate()
+	{
+		transform.rotation = Quaternion.Slerp (startRot, targetRot, Time.deltaTime * Speed);
+	}
+
+	public Quaternion Target (Vector3 targPoint)
+	{
+		Vector3 direction = targPoint - transform.position;
+		direction.Normalize ();
+		
+		float rot_z = Mathf.Atan2 (direction.y, direction.x) * Mathf.Rad2Deg;
+		
+		return Quaternion.Euler (0f, 0f, rot_z - 90);
+	}
 
 }
---
+++ Project/Assets/Scripts/Tools/Spatial.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Spatial {
+
+	public static bool isAcceptableDistance (float distance, Vector3 bodyOne, Vector3 bodyTwo)
+	{
+		if (Vector3.Distance (bodyOne, bodyTwo) < distance)
+			return true;
+		else
+			return false;
+	}
+}
