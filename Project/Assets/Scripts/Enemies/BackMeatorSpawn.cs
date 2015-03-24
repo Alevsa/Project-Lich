d70abc42381aa1693dbca13e0970a8d09dc2a30e
srowly <srowlands@hotmail.co.uk>
2015-03-24 11:03:21 +0000

    worked on background asteroids.

---
+++ Project/Assets/Scripts/Enemies/BackMeatorSpawn.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class BackMeatorSpawn : MonoBehaviour {
+
+	public GameObject[] Meators;
+	public float Cooldown;
+	public float SpawnChance;
+	
+	private float timeSinceLastSpawn;
+	private float timeSinceLastCheck;
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timeSinceLastCheck > Cooldown) {
+			if (isSpawning())
+			{
+				int j = UnityEngine.Random.Range(1, 2);
+
+				GameObject.Instantiate (Meators[j], this.transform.position + new Vector3 (UnityEngine.Random.Range (-8F, 8F), 0, 0), Quaternion.identity);
+				timeSinceLastSpawn = 0;
+			}
+			timeSinceLastCheck = 0;
+			timeSinceLastSpawn += Time.deltaTime;
+		}
+		else
+		{
+			timeSinceLastCheck += Time.deltaTime;
+			timeSinceLastSpawn += Time.deltaTime;
+		}
+	}
+	
+	bool isSpawning () 
+	{
+		if (((UnityEngine.Random.Range (0F, 1F) / Math.Sqrt (timeSinceLastSpawn)) * 10) <= (SpawnChance / 100))
+			return true;
+		else
+			return false;
+	}
+}
---
+++ Project/Assets/Scripts/Environment/BackAsteroid.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackAsteroid : MonoBehaviour {
+
+	public float lowerSize, upperSize;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		SetSize();
+	}
+	
+	void SetSize()
+	{
+		float size = Random.Range(lowerSize, upperSize);
+		transform.localScale = new Vector3(size, size, 0);
+		
+		GetComponent<ParentAsteroid> ().Speed *= size;
+	}
+}
