78362f98e4d73271420b93b9fb8d69406e467596
Sam Rowlands <srowlands@hotmail.co.uk>
2015-04-13 19:51:18 +0100

    began work on spinner enemy.

---
+++ Project/Assets/Scripts/Enemies/Spinner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spinner : MonoBehaviour {
+
+	public float baseRotSpeed, speedToFire, incSpeed;
+	private float speed;
+	private bool firing;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.forward * (speed * Time.deltaTime));
+		
+		if (firing)
+			speed += (Time.deltaTime * incSpeed);
+		if (speed > speedToFire)
+			fire ();
+		if (!firing && speed > baseRotSpeed)
+			speed -= (Time.deltaTime * incSpeed);
+		
+	}
+	
+	public void startFire()
+	{
+		firing = true;
+	}
+	
+	private void fire()
+	{
+		firing = false;
+	}
+}
--- Project/Assets/Scripts/Enemies/SprayEnemy.cs
+++ Project/Assets/Scripts/Enemies/SprayEnemy.cs
@@ -3,21 +3,45 @@ using System.Collections;
 
 public class SprayEnemy : Enemy {
 
+	public float yDisplace, timeBeforeMove;
+	private float timer;
+	private Vector3 moveLoc;
+	private bool notFired;
+	private Spinner spinner;
 	// Use this for initialization
 	void Start () {
-	
+		Spinner = GetComponentInChildren<Spinner> ();
+		moveInfrontPlayer ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		timer += Time.deltaTime;
+		
+		if (timer > timeBeforeMove) 
+		{
+			moveInfrontPlayer ();
+			timer = 0f;
+		}
+		
+		if(moveLoc != transform.position)
+			transform.position = Vector3.MoveTowards (transform.position, moveLoc, Speed * Time.deltaTime);
+		
+		if (moveLoc == transform.position && notFired)
+			sprayFire ();
 	}
-
+	
 	private void moveInfrontPlayer()
 	{
+		Vector3 playerPos = GameObject.Find("Player").transform.position;
+		moveLoc = playerPos - new Vector3 (0, yDisplace);
+		notFired = true;
 	}
-
+	
 	private void sprayFire()
 	{
+		notFired = false;
+		spinner.startFire ();
 	}
+
 }
